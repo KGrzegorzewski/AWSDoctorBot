74c94f454f0f8e47e8411505ee71398478bdf907
Adrian Gabor <adgabor97@gmail.com>
2021-12-02 12:13:49 +0100

    navigation

--- doctorbot/src/App.js
+++ doctorbot/src/App.js
@@ -2,10 +2,31 @@
 import './App.css';
 import React from 'react';
  
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+
+
 function App() {
   return (
     <div>
-
+        <Router>
+        <div className="App">
+          <Navigation />
+          <Routes>
+            <Route path="/about" element={<About/>}>
+            </Route>
+            <Route path="/" element={<Home/>}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 } 
---
+++ doctorbot/src/components/Home.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Form = () => {
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const elements = e.target.elements;
+      const requestData = {
+        username: elements.username.value,
+        password: elements.password.value,
+      };
+      const requestJson = JSON.stringify(requestData);
+      try {
+        const response = await fetch("/path/to/backend", {
+          method: "POST",
+          body: requestJson,
+        });
+        const responseText = await response.text();
+        console.log(responseText);
+      } catch (ex) {
+        console.error("POST - błąd!");
+      }
+    };
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nazwa użytkownika: </label>
+          <input type="text" name="username" />
+        </div>
+        <div>
+          <label>Hasło: </label>
+          <input type="password" name="password" />
+        </div>
+        <div>
+          <input type="submit" value="Zatwierdź!" />
+        </div>
+      </form>
+    );
+  };
+
+export default function Home() {
+    return (
+        <div>    
+            <Form/>
+        </div>
+    );
+  }
